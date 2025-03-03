7345edebad4760e71d7e36980c783f30b5159144
bilow osmoo <bilowosmo@gmail.com>
2025-03-03 07:15:02 +0300

    assigment done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'grace Hopper!';
+const welcomeMessage = `"Welcome to Flatbook, ": 'grace hopper'! ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+let shortGreeting = '"Welcome, ": "g" GRACE HOOPER!!\' ${currentUser.toUpperCase() .slice()}!';
